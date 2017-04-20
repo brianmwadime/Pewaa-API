1b050bba21b62d022e88f0b6ae57f0bfc1081f34
Brian Mwakima <mwadime@fortunekidew.co.ke>
2017-04-20 09:56:22 +0300

    [u] Cashout Schema update

--- schema.sql
+++ schema.sql
@@ -353,7 +353,7 @@ ALTER TABLE wishlists ADD COLUMN is_deleted BOOLEAN NOT NULL DEFAULT false;
 
 ALTER TABLE wishlist_contributors ADD COLUMN is_deleted BOOLEAN NOT NULL DEFAULT false;
 
-ALTER TABLE wishlists ALTER COLUMN cashout_status TYPE varchar NULL;
+ALTER TABLE wishlists ADD COLUMN cashout_status varchar(20) DEFAULT NULL;
 
 --
 -- PostgreSQL database dump complete
